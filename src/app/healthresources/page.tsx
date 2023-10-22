74c38ded99e51bfd91df93d9130628de4f884b1f
Brian Xu <mattx0121@gmail.com>
2023-10-22 15:47:57 -0700

    add footer to healthsources page

--- src/app/healthresources/page.tsx
+++ src/app/healthresources/page.tsx
@@ -2,19 +2,30 @@ import { useTheme } from "next-themes";
 import { twMerge } from 'tailwind-merge';
 import { BaseButton } from "@/components/BaseButton";
 import ArrowIcon from "@/components/ArrowIcon";
+import FooterButtons from "@/components/FooterButtons";
 
 export default function HealthResourcesPage() {
-
     return (
-        <div className={twMerge('flex flex-col items-center mb-5')}>
-            <h1 className={twMerge('text-black text-4xl mb-5 dark:text-white')}>Health Resources</h1>
-            <p>Learn more about gingivitis</p>
-            <div>
-                <BaseButton>
-                    bleeding gums
-                    <ArrowIcon />
-                </BaseButton>
-            </div>
+        <div className={twMerge('flex flex-col p-5 bg-gradient-to-br from-24008C to-641A99 via-9D32A5 h-screen')}>
+            <header>
+                <div className={twMerge('flex flex-col items-center mb-5')}>
+                    <h1 className={twMerge('text-black text-4xl mb-5 dark:text-white')}>Health Resources</h1>
+                </div>
+            </header>
+
+            <main className={twMerge('flex flex-col items-center flex-grow pb-16')}>
+                <p>Learn more about gingivitis</p>
+                <div className={twMerge('mt-4')}>
+                    <BaseButton>
+                        bleeding gums
+                        <ArrowIcon />
+                    </BaseButton>
+                </div>
+            </main>
+
+            <footer>
+                <FooterButtons />
+            </footer>
         </div>
     );
 }
