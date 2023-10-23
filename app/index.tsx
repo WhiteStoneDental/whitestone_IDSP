d82b200d5bb9d8b12c0e89ef14d27e3f93256f6a
Devynn <personaldevynn@gmail.com>
2023-10-23 01:57:11 -0700

    add example page

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Text, View } from "react-native";
-import tw from 'twrnc';
+import tw from '../lib/tailwind.js';
+import { useFonts, Mulish_400Regular } from "@expo-google-fonts/mulish";
+
+import WhitestoneLogo from "../assets/whitestone-black.svg";
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    Mulish_400Regular
+  });
+
   return (
-    <View style={tw`flex-1 items-center justify-center bg-red-200`}>
-      <Text>Open up App.js to start working on your app!!</Text>
+    <View style={tw`flex-1 items-center justify-center`}>
+      <WhitestoneLogo width="30%" height="30%" />
+      <Text style={tw`font-mulish text-navy-blue text-lg`}>Your personal dental assistant</Text>
     </View>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'navy-blue': '#00154C',
+      },
+      fontFamily: {
+        'mulish': ['Mulish_400Regular', 'ui-sans-serif'],
+        'sans': ['ui-sans-serif', 'system-ui'],
+      },
+    },
+  },
+  plugins: [],
+}
+
