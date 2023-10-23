892b86007a66b3b5dfa9c19c5d654bc0590504b3
Devynn <personaldevynn@gmail.com>
2023-10-22 21:16:18 -0700

    initial expo commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+import tw from 'twrnc';
+
+export default function App() {
+  return (
+    <View style={tw`flex-1 items-center justify-center bg-red-200`}>
+      <Text>Open up App.js to start working on your app!!</Text>
+    </View>
+  );
+}
