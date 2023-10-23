3692eb98083de8b557f0cffb072d76d0a7090685
Devynn <personaldevynn@gmail.com>
2023-10-23 03:11:40 -0700

    fix for android

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
-import React from "react";
-import { Text, View, Pressable } from "react-native";
+import { useCallback } from "react";
+import { Text, View } from "react-native";
 import { useDeviceContext, useAppColorScheme } from "twrnc";
 import { useFonts, Mulish_400Regular } from "@expo-google-fonts/mulish";
-import { Link } from "expo-router";
+import { Link } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 
 import tw from '../lib/tailwind.js';
+import Button from "../components/button"
 import WhitestoneLogo from "../assets/whitestone-black.svg";
 
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   useDeviceContext(tw, { withDeviceColorScheme: false });
@@ -16,12 +19,21 @@ export default function App() {
     Mulish_400Regular
   });
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={tw`flex-1 items-center justify-center`}>
+    <View style={tw`flex-1 items-center justify-center`} onLayout={onLayoutRootView}>
+      <WhitestoneLogo width="40%" height="40%" style={tw`py-4`} />
       <Link href="/scan">
-        <Pressable>
-          <WhitestoneLogo width="100%" height="100%" style={tw`py-5`}/>
-        </Pressable>
+          <Text style={tw`font-mulish text-xl`}>Scan</Text>
       </Link>
       <Text style={tw`font-mulish text-navy-blue text-lg`}>Your personal dental assistant</Text>
     </View>
