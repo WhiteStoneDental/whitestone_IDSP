45a8382b49a1ce018b6134f14f694530066b7314
Devynn <personaldevynn@gmail.com>
2023-10-25 02:56:40 -0700

    fix landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Landing() {
     <div 
     className="h-screen flex flex-col items-center justify-center min-h-screen bg-[var(--mainphrase-bg)]"
     >
-      <ClientComponent />
+      <Logo />
       <div className="items-center justify-center text-center">
         <h1 className="text-2xl font-bold mb-4">Welcome to WhiteStone</h1>
         <p className="text-sm">Your personal dental assistant</p>
