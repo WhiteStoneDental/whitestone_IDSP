480f48a5d51c6b6f69c0f15c094d36178b04a486
Devynn <personaldevynn@gmail.com>
2023-11-01 11:42:06 -0700

    add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     extend: {
       colors: {
         'navy-blue': '#00154C',
+        'lighter-mint': '#ECFAF7',
+        'light-lavendar': '#F6DFFF',
+        'pinkish-purple': '#9D32A5',
+        'purple': '#641A99',
+        'blue': '#24008C',
+        'dark-mode': '#312737',
+        'dark-gray': '#251C26',
+        'lighter-gray': '#40304B',
       },
       fontFamily: {
         'mulish': ['Mulish_400Regular', 'ui-sans-serif'],
