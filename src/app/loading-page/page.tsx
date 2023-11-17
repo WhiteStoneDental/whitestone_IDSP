ba7c2db9cb20eac33585800b82c6c1ff9ce02c68
JesseWelk <jwelk@my.bcit.ca>
2023-11-16 23:32:31 -0800

    Made landing page

---
+++ src/app/loading-page/page.tsx
@@ -0,0 +1,19 @@
+import Logo from '@/components/Logo';
+import { twMerge } from 'tailwind-merge';
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div 
+    className={twMerge("h-screen flex flex-col items-center justify-center min-h-screen bg-[var(--mainphrase-bg)]")}
+    >
+      <Logo />
+      <div className="items-center justify-center text-center">
+        <h1 className="text-2xl font-bold mb-4">Welcome to WhiteStone</h1>
+        <p className="text-sm">Your personal dental assistant</p>
+      </div>
+    </div>
+  );
+}
+
+
