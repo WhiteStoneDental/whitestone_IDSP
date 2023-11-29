cdd10b6adda02dc67c3c55a7e687cc5570945b59
Jun <junability@gmail.com>
2023-11-29 01:13:38 -0800

    things aligned

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -42,7 +42,6 @@ export default function HomePage() {
   console.log(history);
 
   return (
-
     <div
       className="flex flex-col items-center p-8  h-full relative overflow-y-auto"
       style={{
@@ -52,35 +51,28 @@ export default function HomePage() {
     >
       {/* <LoginButton/> */}
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1  lg:grid-cols-3 gap-4">
         <div className="bg-purple-100 bg-opacity-60 pl-20 pr-20 p-5 rounded-xl text-center shadow-xl dark:bg-[var(--box-color)]">
           <h3 className="text-black font-bold text-xl mb-3 dark:text-white cursor-pointer">
             History
-
-
           </h3>
           <div className="mx-auto flex items-center justify-center">
             <PastScan />
           </div>
         </div>
 
-
         <div className="bg-purple-100 bg-opacity-90 pl-20 pr-20 p-5 rounded-xl text-center shadow-xl dark:bg-[var(--box-color)]">
-          <h3 className="text-black font-bold text-xl mb-3 dark:white-black cursor-pointer">
+          <h3 className="text-black font-bold text-xl mb-3 dark:text-white cursor-pointer">
             Start Scan
-
-
           </h3>
           <div className="mx-auto flex items-center justify-center">
             <StartScanButton />
           </div>
         </div>
 
-
         <div className="bg-purple-100 bg-opacity-60 pl-20 pr-20 p-5 rounded-xl text-center shadow-xl dark:bg-[var(--box-color)]">
           <h3 className="text-black font-bold text-xl mb-3 dark:text-white cursor-pointer">
             Resources
-
           </h3>
           <div className="mx-auto flex items-center justify-center">
             <ResourceButton />
@@ -94,38 +86,41 @@ export default function HomePage() {
         className="bg-white p-5 rounded-xl h-screen w-full overflow-hidden mt-10 mb-5 dark:bg-[var(--mainphrase-bg)]"
         id="latest-scans"
       >
-        <h3 className="text-black font-bold text-2xl mb-3 dark:text-white">
+        <h3 className="text-black font-bold text-2xl mb-5 dark:text-white">
           Latest Scans
         </h3>
         <div id="latest-scans-content">
           {history.oralScans.slice(0, 4).map((scan) => (
-            <div key={scan.id} className="gap-4 ">
-              <div className="bg-purple-100 bg-opacity-60  pl-20 pr-20 p-5 rounded-sm text-center shadow-lg">
-                <h3 className="text-black font-bold text-xl mb-3 dark:text-white">
+            <div key={scan.id} className="gap-4 mt-1">
+              <div className="  pl-20 pr-20 p-5 ">
+                <h3 className="text-black font-bold text-xl  dark:text-white mb-3">
                   {scan.date}
                 </h3>
                 {scan.diseaseAnalysis && (
                   <>
-                    {scan.diseaseAnalysis.gingivitis && (
-                      <h3 className="text-black font-bold text-xl mb-3 dark:text-white ">
-                        {scan.diseaseAnalysis.gingivitis.count} Gingivitis:{" "}
-                        {scan.diseaseAnalysis.gingivitis.level}
-                      </h3>
-                    )}
-                    {scan.diseaseAnalysis.cavities && (
-                      <h3 className="text-black font-bold text-xl mb-3 dark:text-white ">
-                        {scan.diseaseAnalysis.cavities.count} Cavities:{" "}
-                        {scan.diseaseAnalysis.cavities.level}
-                      </h3>
-                    )}
-                    {scan.diseaseAnalysis.gumRecession && (
-                      <h3 className="text-black font-bold text-xl mb-3 dark:text-white cursor-pointer">
-                        {scan.diseaseAnalysis.gumRecession.count} Gum Recession:{" "}
-                        {scan.diseaseAnalysis.gumRecession.level}
-                      </h3>
-                    )}
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+                      {scan.diseaseAnalysis.gingivitis && (
+                        <h3 className="text-black  text-sm  dark:text-white ">
+                          Gingivitis: {scan.diseaseAnalysis.gingivitis.level} (
+                          {scan.diseaseAnalysis.gingivitis.count})
+                        </h3>
+                      )}
+                      {scan.diseaseAnalysis.cavities && (
+                        <h3 className="text-black text-sm  dark:text-white ">
+                          Cavities: {scan.diseaseAnalysis.cavities.level} (
+                          {scan.diseaseAnalysis.cavities.count})
+                        </h3>
+                      )}
+                      {scan.diseaseAnalysis.gumRecession && (
+                        <h3 className="text-black text-sm  dark:text-white">
+                          Gum Recession:{" "}
+                          {scan.diseaseAnalysis.gumRecession.level} (
+                          {scan.diseaseAnalysis.gumRecession.count})
+                        </h3>
+                      )}
+                    </div>
+                      <hr className="mt-3" />
                   </>
-
                 )}
               </div>
             </div>
