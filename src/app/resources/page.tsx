7f9d581391d6d341bfc103081e16f2cadde74b1c
Devynn Thammavong <personaldevynn@gmail.com>
2023-11-30 23:41:50 -0800

    remove old resource page

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,31 +1,10 @@
 import NavBar from "@/components/NavBar";
 import CollapsibleList from "@/components/CollapsibleListItem";
 import LoginButton from "@/components/LoginButton";
+import Accordion from "@/components/Accordion";
 import { twMerge } from 'tailwind-merge';
 
 export default function ResourcePage() {
-   return (
-       <div
-           className={twMerge("flex flex-col p-5 h-screen")}
-           style={{
-               backgroundImage:
-                   "var(--homepage-gradient, linear-gradient(240deg, #24008C, #9D32A5, #641A99))",
-           }}
-       >
-           <div className={twMerge("flex flex-col items-center mt-20 mb-5 font-bold text-6xl text-center text-white")}>
-               Health Resources
-           </div>
-           <div className={twMerge("flex-grow bg-white p-5 rounded-xl overflow-auto")}>
-               <div className={twMerge("text-center font-bold text-xl mt-5 mb-8")}>
-                   Learn More About Oral Health
-               </div>
-               <CollapsibleList />
-           </div>
-           <div>
-               <NavBar/>
-           </div>
-       </div>
-   );
   return (
     <div
       className={twMerge("flex flex-col p-5 mb-5")}
