c3a5fc0d78187b74c59e80c4ed13c5aa2719404c
Devynn <personaldevynn@gmail.com>
2023-12-01 13:07:13 -0800

    fix Treatments

--- src/app/treatments/page.tsx
+++ src/app/treatments/page.tsx
@@ -1,7 +1,4 @@
-import { useTheme } from "next-themes";
-
-export default function TreatmentsPage() {
-  const { resolvedTheme, setTheme } = useTheme();
+export default function Treatments() {
   return (
     <div className="p-4">
       <h1 className="text-3xl font-bold mb-4">Treatments</h1>
