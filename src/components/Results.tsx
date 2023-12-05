1c8399de46c98080b67d71cf591e319c343d84b2
Jun <junability@gmail.com>
2023-12-04 20:58:09 -0800

    user download result

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -2,7 +2,7 @@
 import NavBar from "@/components/NavBar";
 import Accordion from "@/components/Accordion";
 import LoginButton from "@/components/LoginButton";
-
+import html2pdf from "html2pdf.js";
 import ResultsAccordion from "@/components/ResultsAccordion";
 import { useEffect, useState } from "react";
 import Image from "next/image";
@@ -23,26 +23,61 @@ export default function Results() {
     setImgSrc(imgURL);
   }, []);
 
+
+  const handleDownload = () => {
+    const content = JSON.stringify(results, null, 2);
+    const blob = new Blob([content], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "scan_results.json";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  };
+
+  const handleDownloadPDF = () => {
+    const content = document.getElementById("results-content"); // Specify the ID of the content to convert
+    const pdfOptions = {
+      margin: 10,
+      filename: "scan_results.pdf",
+      image: { type: "jpeg", quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+    };
+
+    html2pdf(content, pdfOptions);
+  };
+
   return (
     <div className="flex flex-col p-5 gradient-bg">
       <div className="flex flex-col items-center mt-20 mb-5 font-bold text-6xl text-center text-white">
         Scan Results
       </div>
       <div className="bg-white p-5 rounded-xl h-full w-full overflow-hidden mb-10 dark:bg-[var(--mainphrase-bg)]">
-        
         {imgSrc && (
           <div className="mx-auto flex items-center justify-center mb-5">
             <Image src={imgSrc} alt="webcam image" width={400} height={400} />
           </div>
         )}
         <hr className="border-t border-white dark:border-white border-solid my-5"></hr>
+        <div className="flex justify-center">
+          {/* Download button */}
+          <button
+          onClick={handleDownloadPDF}
+          className="border mb-2  hover:bg-purple-900 text-white font-bold py-2 px-4 rounded"
+        >
+          Download as PDF
+        </button>
+        </div>
         <div className="placeholder-content text-black dark:text-white text-center font-bold text-xl mb-5">
           {results.date}
         </div>
         <ResultsAccordion results={results} />
-      <div>
-        <NavBar />
-      </div>
+        <div>
+          <NavBar />
+        </div>
       </div>
     </div>
   );
