77db2b71cdf091bb0bdc2445f7a2eadde2690eb9
Jun <junability@gmail.com>
2023-12-07 20:45:53 -0800

    padding

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -2,7 +2,7 @@
 import NavBar from "@/components/NavBar";
 import { useState, useEffect } from "react";
 import NoScans from "@/components/NoScans";
-  
+
 type Issue = {
   id: string;
   issue_title: string;
@@ -18,18 +18,18 @@ type OpenAIResult = {
   error?: string;
 };
 
-type IssueSeverity = 'red' | 'orange' | 'yellow';
+type IssueSeverity = "red" | "orange" | "yellow";
 
 const getDotColor = (severity: IssueSeverity) => {
   switch (severity) {
-    case 'red':
-      return 'bg-red-500';
-    case 'orange':
-      return 'bg-orange-500';
-    case 'yellow':
-      return 'bg-yellow-400';
+    case "red":
+      return "bg-red-500";
+    case "orange":
+      return "bg-orange-500";
+    case "yellow":
+      return "bg-yellow-400";
     default:
-      return '';
+      return "";
   }
 };
 
@@ -51,8 +51,8 @@ export default function History() {
   return (
     <div className="flex flex-col gradient-bg overflow-y-auto   items-center relative p-8">
       <div
-      className="bg-white p-5 rounded-xl h-screen w-9/12 overflow-hidden mt-10 mb-5 dark:bg-[var(--mainphrase-bg)]"
-      id="history"
+        className="bg-white p-5 rounded-xl h-screen w-9/12 overflow-hidden mt-10 mb-5 dark:bg-[var(--mainphrase-bg)]"
+        id="history"
       >
         <h1 className="text-black font-bold text-center text-2xl mb-5 dark:text-white">
           History
@@ -60,7 +60,7 @@ export default function History() {
         <div id="all-scans-content ">
           {results ? (
             <div className="gap-4 mt-1">
-              <div className="pl-20 pr-20 p-5">
+              <div className="p-5 sm:pl-20 sm:pr-20">
                 <h3 className="text-black font-bold text-sm sm:text-xl sm:font-bold dark:text-white mb-3">
                   {results.date}
                 </h3>
@@ -69,13 +69,15 @@ export default function History() {
                     <div className="grid grid-cols-1  gap-2">
                       {results.mild.map((issue) => (
                         <div key={issue.id}>
-                          <div className={`rounded-full w-3 h-3 inline-block ${getDotColor('yellow')} mr-2`} />
+                          <div
+                            className={`rounded-full w-3 h-3 inline-block ${getDotColor(
+                              "yellow"
+                            )} mr-2`}
+                          />
                           <span className="font-bold text-black text-sm dark:text-white mb-3">
                             Mild Issue: {issue.issue_title}
                           </span>
-                          <h3
-                            className="text-black text-sm dark:text-white mb-3"
-                          >
+                          <h3 className="text-black text-sm dark:text-white mb-3">
                             {issue.issue_description}
                           </h3>
                         </div>
@@ -88,13 +90,15 @@ export default function History() {
                     <div className="grid grid-cols-1 gap-2">
                       {results.moderate.map((issue) => (
                         <div key={issue.id}>
-                          <div className={`rounded-full w-3 h-3 inline-block ${getDotColor('orange')} mr-2`} />
+                          <div
+                            className={`rounded-full w-3 h-3 inline-block ${getDotColor(
+                              "orange"
+                            )} mr-2`}
+                          />
                           <span className="font-bold text-black text-sm dark:text-white mb-3">
                             Moderate Issue: {issue.issue_title}
                           </span>
-                          <h3
-                            className="text-black text-sm dark:text-white mb-3"
-                          >
+                          <h3 className="text-black text-sm dark:text-white mb-3">
                             {issue.issue_description}
                           </h3>
                         </div>
@@ -107,13 +111,15 @@ export default function History() {
                     <div className="grid grid-cols-1  gap-2">
                       {results.severe.map((issue) => (
                         <div key={issue.id}>
-                          <div className={`rounded-full w-3 h-3 inline-block ${getDotColor('red')} mr-2`} />
+                          <div
+                            className={`rounded-full w-3 h-3 inline-block ${getDotColor(
+                              "red"
+                            )} mr-2`}
+                          />
                           <span className="font-bold text-black text-sm dark:text-white mb-3">
                             Severe Issue: {issue.issue_title}
                           </span>
-                          <h3
-                            className="text-black text-sm dark:text-white mb-3"
-                          >
+                          <h3 className="text-black text-sm dark:text-white mb-3">
                             {issue.issue_description}
                           </h3>
                         </div>
@@ -124,11 +130,11 @@ export default function History() {
                 <hr className="mt-3" />
               </div>
             </div>
-  ) : (
-    <NoScans />
-  )}
-</div>
-</div>
+          ) : (
+            <NoScans />
+          )}
+        </div>
+      </div>
       <div className="mt-auto">
         <NavBar />
       </div>
